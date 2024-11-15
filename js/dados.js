7e4c81eb884ab79af2a5b2fe7879f9d30d86281c
monsterdigital-ux <vieira.artoficial@gmail.com>
2024-11-15 15:58:18 -0300

    Add files via upload

---
+++ js/dados.js
@@ -0,0 +1,348 @@
+var MENU = {
+    "churrasco": [
+        {
+            "id": "ribs-brisket-and-burnt-ends",
+            "img": "images/caneca 1.jpg",
+            "name": "estampa",
+            "dsc": "Joe's KC Ribs, Brisket & Burnt Ends",
+            "price": 110.99
+        },
+        {
+            "id": "005-kings-carolina-oink-sampler",
+            "img": "images/caneca 2.jpg",
+            "name": "estampa",
+            "dsc": "Carolina BBQ Oink Sampler",
+            "price": 89
+        },
+        {
+            "id": "texas-monthlys-1-bbq-brisket",
+           "img": "images/caneca 3.jpg",
+            "name": "estampa",
+            "dsc": "Texas Monthly's #1 BBQ Brisket",
+            "price": 199
+        },
+        {
+            "id": "whole-brisket-texas-bbq-sauce",
+            "img": "images/caneca 4.jpg",
+            "name": "estampa",
+            "dsc": "Whole Brisket + Texas Barbecue BBQ Sauce",
+            "price": 249
+        },
+        {
+            "id": "whole-texas-smoked-brisket",
+           "img": "images/caneca 5.jpg",
+            "name": "estampa",
+            "dsc": "Whole Texas Smoked Brisket",
+            "price": 189
+        },
+        {
+            "id": "mini-trinity-bbq-combo-brisket-ribs-and-links",
+            "img": "images/caneca 6.jpg",
+            "name": "estampa",
+            "dsc": "Mini Trinity BBQ Combo - Brisket, Ribs & Links",
+            "price": 139
+        },
+        {
+            "id": "235203-blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo",
+           "img": "images/caneca 7.jpg",
+            "name": "estampa",
+            "dsc": "Blue Smoke Baby Back Ribs + Backyard Barbecue Chicken Combo",
+            "price": 129
+        },
+        {
+            "id": "006-kings-meat-lovers-special",
+           "img": "images/caneca 8.jpg",
+            "name": "estampa",
+            "dsc": "BBQ Meat Lovers Special for 10",
+            "price": 139
+        },
+        {
+            "id": "the-big-ugly-bbq-dinner-for-6",
+         "img": "images/caneca 9.jpg",
+            "name": "estampa",
+            "dsc": "The Big Ugly BBQ Dinner for 6",
+            "price": 229
+        },
+        {
+            "id": "17796-mighty-quinns-bbq-sampler-pack",
+           "img": "images/caneca 10.jpg",
+            "name": "estampa",
+            "dsc": "Mighty Quinn's BBQ Sampler Pack",
+            "price": 169
+        },
+        {
+            "id": "post-oak-smoked-half-brisket",
+          "img": "images/caneca 11.jpg",
+            "name": "estampa",
+            "dsc": "Post Oak Smoked USDA Prime Half Brisket",
+            "price": 109
+        },
+        {
+            "id": "best-of-texas-bbq-combo-serves-14",
+           "img": "images/caneca 12.jpg",
+            "name": "estampa",
+            "dsc": "Best of Texas BBQ Combo - Serves 14",
+            "price": 269
+        }
+    ],
+    "burgers": [
+        {
+            "id": "the-gramercy-tavern-burger-4-pack",
+            "img": "images/meia 1.jpg",
+            "name": "Estampa",
+            "dsc": "The Gramercy Tavern Burger - 4 Pack",
+            "price": 99
+        },
+        {
+            "id": "shake-shack-shackburger-8-pack",
+            "img": "images/meia 2.jpg",
+            "name": "Estampa",
+            "dsc": "Shake Shack ShackBurger® - 8 Pack",
+            "price": 49
+        },
+        {
+            "id": "gotts-cheeseburger-kit-for-4",
+            "img": "images/meia 3.jpg",
+            "name": "Estampa",
+            "dsc": "Gott's Complete Cheeseburger Kit for 4",
+            "price": 99
+        },
+        {
+            "id": "le-big-matt-kit-for-6",
+            "img":  "images/meia 4.jpg",
+            "name": "Estampa",
+            "dsc": "Le Big Matt Burger Kit for 6",
+            "price": 99
+        },
+        {
+            "id": "shake-shack-shackburger-16-pack",
+            "img": "images/meia 5.jpg",
+            "name": "Estampa",
+            "dsc": "Shake Shack Shackburger® - 16 Pack",
+            "price": 89
+        },
+        {
+            "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
+            "img":  "images/meia 6.jpg",
+            "name": "Estampa",
+            "dsc": "USDA Prime Burgers - Pack of 18 (8oz each)",
+            "price": 175.95
+        },
+        {
+            "id": "double-stack-burger-kit-for-4",
+            "img":  "images/meia 7.jpg",
+            "name": "Estampa",
+            "dsc": "Double Stack Burger Kit for 4",
+            "price": 79
+        },
+        {
+            "id": "goldbelly-burger-bash-pack",
+            "img":  "images/meia 8.jpg",
+            "name": "Estampa",
+            "dsc": "Goldbelly \"Burger Bash\" Pack",
+            "price": 109
+        },
+        {
+            "id": "burger-au-poivre-kit-4-pack",
+            "img": "images/meia 9.jpg",
+            "name": "Estampa",
+            "dsc": "Burger Au Poivre Kit - 4 Pack",
+            "price": 99
+        },
+        {
+            "id": "goldbelly-burger-blend-4-lbs",
+            "img":  "images/meia 10.jpg",
+            "name": "Estampa",
+            "dsc": "Goldbelly Burger Blend - 4 lbs.",
+            "price": 79
+        },
+        {
+            "id": "gotts-complete-cheeseburger-kit-for-8",
+            "img":  "images/meia 11.jpg",
+            "name": "Estampa",
+            "dsc": "Gott's Complete Cheeseburger Kit for 8",
+            "price": 149
+        },
+        {
+            "id": "gramercy-tavern-burger-kielbasa-combo",
+            "img":  "images/meia 12.jpg",
+            "name": "Estampa",
+            "dsc": "Gramercy Tavern Burger + Kielbasa Combo",
+            "price": 149
+        }
+    ],
+   
+    
+    "pizzas": [
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+"img": "images/chinelo 1.jpg", 
+            "name": "estampa",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 67.99
+        },
+        {
+            "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
+"img": "images/chinelo 2.jpg", 
+            "name": "estampa",
+            "dsc": "Choose Your Own Thin Crust Pizza - 4 Pack",
+            "price": 139
+        },
+        {
+            "id": "choose-your-own-new-haven-style-pizza-6-pack",
+"img": "images/chinelo 3.jpg", 
+            "name": "estampa",
+            "dsc": "New Haven-Style Pizza - 6 Pack (Choose Your Own)",
+            "price": 79
+        },
+        {
+            "id": "6-lou-malnatis-deep-dish-pizzas",
+"img": "images/chinelo 4.jpg", 
+            "name": "estampa",
+            "dsc": "6 Lou Malnati's Deep Dish Pizzas",
+            "price": 116.99
+        },
+        {
+            "id": "wood-fired-pizzas-best-seller-4-pack",
+"img": "images/chinelo 5.jpg", 
+            "name": "estampa",
+            "dsc": "Wood Fired Pizzas Best Seller - 4 Pack",
+            "price": 129
+        },
+        {
+            "id": "236991-choose-your-own-deep-dish-pizza-3-pack",
+"img": "images/chinelo 6.jpg", 
+            "name": "estampa",
+            "dsc": "Choose Your Own Deep Dish Pizza - 3 Pack",
+            "price": 139
+        },
+        {
+            "id": "choose-your-own-detroit-style-pizza-3-pack",
+"img": "images/chinelo 7.jpg", 
+            "name": "estampa",
+            "dsc": "Detroit-Style Pizza - Choose Your Own 3 Pack",
+            "price": 89
+        },
+        {
+            "id": "brooklyn-pizza-choose-your-own-5-pack",
+"img": "images/chinelo 8.jpg", 
+            "name": "estampa",
+            "dsc": "Brooklyn Pizza - Choose Your Own 5 Pack",
+            "price": 69
+        },
+        {
+            "id": "choose-your-own-chicago-deep-dish-pizza-4-pack",
+"img": "images/chinelo 9.jpg", 
+            "name": "estampa",
+            "dsc": "Chicago Deep Dish Pizza - 4 Pack",
+            "price": 129
+        },
+        {
+            "id": "4-lou-malnatis-deep-dish-pizzas",
+"img": "images/chinelo 10.jpg", 
+            "name": "estampa",
+            "dsc": "4 Lou Malnati's Deep Dish Pizzas",
+            "price": 96.99
+        },
+        {
+            "id": "tonys-custom-pizza-3-pack",
+"img": "images/chinelo 11.jpg", 
+            "name": "estampa",
+            "dsc": "Choose Your Own Pizza - 3 Pack",
+            "price": 99
+        },
+        {
+            "id": "plain-thin-crust-pizza-4-pack",
+"img": "images/chinelo 12.jpg", 
+            "name": "estampa",
+            "dsc": "Plain Thin Crust Pizza - 4 Pack",
+            "price": 79
+        }
+    ],
+    "steaks": [
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/california-reserve-filet-mignon-steaks-gift-box.bf226e317aad85f47897ae7e325f790d.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+        {
+            "id": "steaks-and-cakes-date-night-dinner-for-2",
+            "img": "./img/cardapio/steaks/valentines-steaks-and-cakes-dinner-for-2.0c63dab635eed46209455dc33cd25ea8.jpg",
+            "name": "Chesapeake Bay Gourmet",
+            "dsc": "Steaks and Cakes Date Night Dinner for 2",
+            "price": 129
+        },
+        {
+            "id": "Prime-holiday-steak-sampler-for-10-12",
+            "img": "./img/cardapio/steaks/holiday-sampler-9-pack.2de75ca80282ffe9d064eb757ff7a5a1.jpg",
+            "name": "Saltbrick Prime",
+            "dsc": "Chef Matt's Steak Sampler for 10-12",
+            "price": 179
+        },
+        {
+            "id": "bone-in-rib-steak",
+            "img": "./img/cardapio/steaks/bone-in-rib-steak.b13d9d4233035767605f0de9acdce1ab.jpg",
+            "name": "Old Homestead Steakhouse",
+            "dsc": "Bone-in Rib Steak",
+            "price": 159
+        },
+        {
+            "id": "american-wagyu-gold-grade-top-sirloins",
+            "img": "./img/cardapio/steaks/american-wagyu-gold-grade-sirloins.040dbeb8f8e615b91fa7e513e3dc089c.jpg",
+            "name": "Snake River Farms",
+            "dsc": "American Wagyu Gold Grade Top Sirloins",
+            "price": 119
+        },
+        {
+            "id": "2-peter-luger-steak-pack-b",
+            "img": "./img/cardapio/steaks/peter-luger-steak-pack-b.9feb0300e6be2dfecfa314f2006a2183.jpg",
+            "name": "Peter Luger Steak House",
+            "dsc": "Peter Luger Porterhouse Steaks",
+            "price": 215.95
+        },
+        {
+            "id": "ribeye-prime-steak-gift-box",
+            "img": "./img/cardapio/steaks/ribeye-prime-steak-gift-box.e74cb016baabbb2df73861de8150f29c.jpg",
+            "name": "Churchill's Steakhouse",
+            "dsc": "Ribeye Prime Steak Gift Box",
+            "price": 229
+        },
+        {
+            "id": "dry-aged-usda-prime-black-angus-porterhouse-steak-2-pack",
+            "img": "./img/cardapio/steaks/usda-prime-black-angus-filet-mignon-barrel-cut.6ee213799e7d1848763d12edca18e3b1.jpg",
+            "name": "Pat LaFrieda Meats",
+            "dsc": "Dry-Aged USDA Prime Black Angus Porterhouse Steak - 2 Pack",
+            "price": 96.7
+        },
+        {
+            "id": "california-reserve-ribeye-steak",
+            "img": "./img/cardapio/steaks/california-reserve-ribeye-steak-12-oz.d367c5ae72dd9f89e170662104bef4fc.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Ribeye Steak - 12 oz",
+            "price": 32
+        },
+        {
+            "id": "dry-aged-boneless-ribeye-steak-dinner-kit-for-4",
+            "img": "./img/cardapio/steaks/dry-aged-boneless-ribeye-steak-dinner-for-4.81c3bdc05fe6bdb2c2214709863120e0.jpg",
+            "name": "Chef Francis Mallmann",
+            "dsc": "Dry-Aged Boneless Ribeye Steak Dinner Kit for 4",
+            "price": 225
+        },
+        {
+            "id": "california-reserve-filet-mignon-steak",
+            "img": "./img/cardapio/steaks/california-reserve-filet-mignon-steak.ff15071964ec8141d30c2ba05fb117e0.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Filet Mignon Steak",
+            "price": 22
+        },
+        {
+            "id": "mesquite-smoked-peppered-beef-tenderloin",
+            "img": "./img/cardapio/steaks/mesquite-smoked-peppered-beef-tenderloin.5c314418a1f75c7057eed686e2fad46f.jpg",
+            "name": "Perini Ranch Steakhouse",
+            "dsc": "Mesquite Smoked Peppered Beef Tenderloin",
+            "price": 165
+        }
+    ]
+}
